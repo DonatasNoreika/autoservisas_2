d3d24f33ddf71cad0966e1a5dae7b5c60316480e
Donoras <donoras@gmail.com>
2021-05-05 11:26:55 +0300

    4-3/4/5 done

--- mysite/autoservice/views.py
+++ mysite/autoservice/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Service, Car, Order
 
 # Create your views here.
+
+def info(request):
+    num_services = Service.objects.all().count()
+    num_cars = Car.objects.all().count()
+    num_orders_done = Order.objects.filter(status__exact='d').count()
+
+    context = {
+        'num_services': num_services,
+        'num_cars': num_cars,
+        'num_orders_done': num_orders_done,
+    }
+    return render(request, 'info.html', context=context)
