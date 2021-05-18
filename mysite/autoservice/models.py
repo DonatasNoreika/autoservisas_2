cbb9fc621bd5073a1518c156e715cb7a789052f6
Donoras <donoras@gmail.com>
2021-05-18 10:50:24 +0300

    11-2 models translations

--- mysite/autoservice/models.py
+++ mysite/autoservice/models.py
@@ -3,59 +3,59 @@ from django.contrib.auth.models import User
 from datetime import datetime
 from tinymce.models import HTMLField
 from PIL import Image
+from django.utils.translation import gettext_lazy as _
 
 import pytz
-
 utc = pytz.UTC
 
 
 # Create your models here.
 
 class Service(models.Model):
-    name = models.CharField('Name', max_length=200)
-    price = models.FloatField("Price")
+    name = models.CharField(verbose_name=_('Name'), max_length=200)
+    price = models.FloatField(verbose_name=_("Price"))
 
     def __str__(self):
         return self.name
 
     class Meta:
-        verbose_name = 'Service'
-        verbose_name_plural = 'Services'
+        verbose_name = _('Service')
+        verbose_name_plural = _('Services')
 
 
 class Car(models.Model):
-    manufacturer = models.CharField('Manufacturer', max_length=200)
-    model = models.CharField('Model', max_length=200)
-    engine = models.CharField('Engine', max_length=200)
+    manufacturer = models.CharField(_('Manufacturer'), max_length=200)
+    model = models.CharField(_('Model'), max_length=200)
+    engine = models.CharField(_('Engine'), max_length=200)
 
     def __str__(self):
         return f"{self.manufacturer} {self.model}, {self.engine}"
 
     class Meta:
-        verbose_name = 'Car'
-        verbose_name_plural = 'Cars'
+        verbose_name = _('Car')
+        verbose_name_plural = _('Cars')
 
 
 class OwnerCar(models.Model):
-    year = models.IntegerField('Year', null=True)
-    owner = models.ForeignKey(User, verbose_name="Owner", on_delete=models.SET_NULL, null=True, blank=True)
-    car = models.ForeignKey('Car', verbose_name="Model", on_delete=models.SET_NULL, null=True)
-    licence_plate = models.CharField('Licence plate', max_length=200)
-    vin_code = models.CharField('VIN code', max_length=200)
-    photo = models.ImageField('Photo', upload_to='cars', null=True)
-    description = HTMLField("Description", null=True)
+    year = models.IntegerField(_('Year'), null=True)
+    owner = models.ForeignKey(User, verbose_name=_("Owner"), on_delete=models.SET_NULL, null=True, blank=True)
+    car = models.ForeignKey('Car', verbose_name=_("Model"), on_delete=models.SET_NULL, null=True)
+    licence_plate = models.CharField(_('Licence plate'), max_length=200)
+    vin_code = models.CharField(_('VIN code'), max_length=200)
+    photo = models.ImageField(_('Photo'), upload_to='cars', null=True)
+    description = HTMLField(_("Description"), null=True)
 
     def __str__(self):
         return f"{self.owner}: {self.car}, {self.licence_plate}, {self.vin_code}"
 
     class Meta:
-        verbose_name = 'Owner Car'
-        verbose_name_plural = 'Owners Cars'
+        verbose_name = _('Owner Car')
+        verbose_name_plural = _('Owners Cars')
 
 
 class Order(models.Model):
-    owner_car = models.ForeignKey('OwnerCar', verbose_name="Model", on_delete=models.SET_NULL, null=True)
-    due_date = models.DateTimeField('Due Date', null=True, blank=True)
+    owner_car = models.ForeignKey('OwnerCar', verbose_name=_("Model"), on_delete=models.SET_NULL, null=True)
+    due_date = models.DateTimeField(verbose_name=_('Due Date'), null=True, blank=True)
 
     @property
     def final(self):
@@ -75,14 +75,14 @@ class Order(models.Model):
         return False
 
     class Meta:
-        verbose_name = 'Order'
-        verbose_name_plural = 'Orders'
+        verbose_name = _('Order')
+        verbose_name_plural = _('Orders')
 
     STATUS = (
-        ('r', 'Draft'),
-        ('i', 'In progress'),
-        ('d', 'Done'),
-        ('c', 'Canceled'),
+        ('r', _('Draft')),
+        ('i', _('In progress')),
+        ('d', _('Done')),
+        ('c', _('Canceled')),
     )
 
     status = models.CharField(
@@ -90,22 +90,22 @@ class Order(models.Model):
         choices=STATUS,
         blank=True,
         default='r',
-        help_text='Status',
+        help_text=_('Status'),
     )
 
 
 class OrderLine(models.Model):
-    order = models.ForeignKey('Order', verbose_name="Order", on_delete=models.SET_NULL, null=True, related_name='lines')
-    service = models.ForeignKey('Service', verbose_name="Service", on_delete=models.SET_NULL, null=True)
-    qty = models.IntegerField("Quantity")
+    order = models.ForeignKey('Order', verbose_name=_("Order"), on_delete=models.SET_NULL, null=True, related_name='lines')
+    service = models.ForeignKey('Service', verbose_name=_("Service"), on_delete=models.SET_NULL, null=True)
+    qty = models.IntegerField(_("Quantity"))
 
     @property
     def total(self):
         return self.service.price * self.qty
 
     class Meta:
-        verbose_name = 'Order Line'
-        verbose_name_plural = 'Order Lines'
+        verbose_name = _('Order Line')
+        verbose_name_plural = _('Order Lines')
 
     def __str__(self):
         return f"{self.order}: {self.service}, {self.qty}"
@@ -114,13 +114,17 @@ class OrderLine(models.Model):
 class OrderComment(models.Model):
     order = models.ForeignKey('Order', on_delete=models.SET_NULL, null=True, blank=True)
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
-    date_created = models.DateTimeField(auto_now_add=True)
-    content = models.TextField('Comment', max_length=2000)
+    date_created = models.DateTimeField(verbose_name=_("Date"), auto_now_add=True)
+    content = models.TextField(_('Comment'), max_length=2000)
+
+    class Meta:
+        verbose_name = _('Comment')
+        verbose_name_plural = _('Comments')
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    photo = models.ImageField(default="default.png", upload_to="profile_pics")
+    photo = models.ImageField(verbose_name=_("Photo"), default="default.png", upload_to="profile_pics")
 
     def __str__(self):
         return f"{self.user.username} profile"
