3d93d11c2fe8186956f74a27ced62d896f0a054d
Donoras <donoras@gmail.com>
2021-05-19 20:19:14 +0300

    12 frontend fixes

--- mysite/autoservice/models.py
+++ mysite/autoservice/models.py
@@ -53,6 +53,14 @@ class OwnerCar(models.Model):
         verbose_name = _('Owner Car')
         verbose_name_plural = _('Owners Cars')
 
+    def save(self):
+        super().save()
+        img = Image.open(self.photo.path)
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.photo.path)
+
 
 class Order(models.Model):
     owner_car = models.ForeignKey('OwnerCar', verbose_name=_("Model"), on_delete=models.SET_NULL, null=True)
